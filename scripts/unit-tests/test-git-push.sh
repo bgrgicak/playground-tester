8f5042a95c9741358185148a0e35a182dcee9e84
Bero <berislav.grgicak@gmail.com>
2024-12-20 08:02:35 +0100

    Try with GH

--- scripts/unit-tests/test-git-push.sh
+++ scripts/unit-tests/test-git-push.sh
@@ -3,19 +3,26 @@
 # Navigate to the data submodule
 cd data || exit 1
 
-REPO_URL="https://github.com/bgrgicak/Playground-compatibility-reports.git"
-if [ -n "$GH_TOKEN" ]; then
-    echo "Using GH_TOKEN to authenticate."
-    REPO_URL="https://${GH_TOKEN}@github.com/bgrgicak/Playground-compatibility-reports.git"
+REPO_URL="origin"
+#"https://github.com/bgrgicak/Playground-compatibility-reports.git"
+if [ -n "$GH_TOKEN" ] && [ -n "$GH_USER" ]; then
+    echo "Logging in with GH_TOKEN"
+    echo "$GH_TOKEN" | gh auth login --with-token
 fi
 
 # Test git push with --dry-run (with any pending changes)
-push_output=$(git push --dry-run $REPO_URL main 2>&1)
+push_output=$(git push "$REPO_URL" --dry-run main 2>&1)
 push_status=$?
 
+echo "Push output: $push_output"
+echo "Push status: $push_status"
+
 # Test git push with --dry-run (should show up-to-date)
-no_change_output=$(git push --dry-run $REPO_URL main 2>&1)
+no_change_output=$(git push --dry-run "$REPO_URL" main 2>&1)
 no_change_status=$?
+
+echo "No change output: $no_change_output"
+echo "No change status: $no_change_status"
 
 # Assert results
 if [ $push_status -eq 0 ] && \
