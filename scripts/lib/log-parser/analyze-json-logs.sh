638f109afe8cf110774b104367fcee0c32498a82
Bero <berislav.grgicak@gmail.com>
2024-12-26 12:48:50 +0100

    Fix missing space in get_first_n_logs_to_test

--- scripts/lib/log-parser/analyze-json-logs.sh
+++ scripts/lib/log-parser/analyze-json-logs.sh
@@ -57,7 +57,7 @@ get_first_n_logs_to_test() {
     local item_type="$1"
     local batch_size="$2"
 
-    get_log_files "$item_type" -name"*last-tested.txt" |
+    get_log_files "$item_type" -name "*last-tested.txt" |
         awk -F'/' '{print $NF " " substr($0, 1, length($0)-length($NF)-1)}' |
         sort |
         cut -d' ' -f2- |
