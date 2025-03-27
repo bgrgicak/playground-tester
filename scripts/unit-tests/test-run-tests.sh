c68f0268ca90beb48cc01fd1c433c3279759227c
Jan Jakes <jan@jakes.pro>
2025-03-27 21:28:23 +0100

    Fix tests

--- scripts/unit-tests/test-run-tests.sh
+++ scripts/unit-tests/test-run-tests.sh
@@ -8,15 +8,18 @@ source "$PLAYGROUND_TESTER_PATH/scripts/unit-tests/common-test-functions.sh"
 
 run_test "Test a successful theme run"\
     "./scripts/run-tests.sh --theme $PLAYGROUND_TESTER_DATA_PATH/logs/themes/g/100-bytes --wordpress ./temp/wordpress" \
-    "✓ 100-bytes passed asyncify-boot
+    "✓ 100-bytes passed ast-sqlite-boot
+✓ 100-bytes passed asyncify-boot
 ✓ 100-bytes passed jspi-boot"
 
 run_test "Test a successful plugin run"\
     "./scripts/run-tests.sh --plugin $PLAYGROUND_TESTER_DATA_PATH/logs/plugins/0/0gravatar --wordpress ./temp/wordpress" \
-    "✓ 0gravatar passed asyncify-boot
+    "✓ 0gravatar passed ast-sqlite-boot
+✓ 0gravatar passed asyncify-boot
 ✓ 0gravatar passed jspi-boot"
 
 run_test "Test a failed plugin run"\
     "./scripts/run-tests.sh --plugin $PLAYGROUND_TESTER_DATA_PATH/logs/plugins/g/google-maps-effortless --wordpress ./temp/wordpress" \
-    "✗ google-maps-effortless failed asyncify-boot
-✗ google-maps-effortless failed jspi-boot"
+    "✗ gl-import-external-images failed ast-sqlite-boot
+✗ gl-import-external-images failed asyncify-boot
+✗ gl-import-external-images failed jspi-boot"
