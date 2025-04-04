93d4fb6dc246d36db6f441931dedb4083f025027
Jan Jakes <jan@jakes.pro>
2025-04-04 15:12:57 +0200

    Retry pushes up to 10 times

--- scripts/save-data.sh
+++ scripts/save-data.sh
@@ -58,7 +58,7 @@ save_data() {
 
   if $push; then
     # Try pull & push multiple times to avoid rejected push errors.
-    local max_attempts=5
+    local max_attempts=10
     local attempt=1
     while [ $attempt -le $max_attempts ]; do
       # Pull remote changes, allow unrelated histories and automatically accept remote version for conflicts
